e85820e63c9bb833046992c5fb099ff9f9749614
Sabrina Herrero <sabrinaherrero123@gmail.com>
2019-06-01 14:39:32 -0400

    initial setup for the tabs

---
+++ Memories/Ideas/IdeasViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IdeasViewController.swift
+//  Memories
+//
+//  Created by Sabrina Herrero on 6/1/19.
+//  Copyright © 2019 Sabrina Herrero. All rights reserved.
+//
+
+import UIKit
+
+class IdeasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Memories/Pictures/PicturesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PicturesViewController.swift
+//  Memories
+//
+//  Created by Sabrina Herrero on 6/1/19.
+//  Copyright © 2019 Sabrina Herrero. All rights reserved.
+//
+
+import UIKit
+
+class PicturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
